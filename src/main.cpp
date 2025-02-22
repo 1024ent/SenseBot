f9bb33b53ca1b148ea8b14ec4b1b6fd51c0d19e6
1024ent <shonenent1024@gmail.com>
2025-02-22 23:20:45 +0800

    Added movement functions

--- src/main.cpp
+++ src/main.cpp
@@ -11,17 +11,25 @@
 // TB6612FNG STBY PIN
 #define   STBY        10
 
+// TB6612FNG PWM PIN LEFT MOTOR
+#define   LEFT_PWM    13
+const int pwmChannel_left = 1;       // PWM channel (0-15)
+const int pwmFrequency_left = 5000;  // Frequency in Hz
+const int pwmResolution_left = 8;    // Resolution in bits (0-255)
+
 // TB6612FNG PWM PIN RIGHT MOTOR
 #define   RIGHT_PWM   4
 const int pwmChannel_right = 0;       // PWM channel (0-15)
 const int pwmFrequency_right = 5000;  // Frequency in Hz
 const int pwmResolution_right = 8;    // Resolution in bits (0-255)
 
-// TB6612FNG PWM PIN LEFT MOTOR
-#define   LEFT_PWM    13
-const int pwmChannel_left = 1;       // PWM channel (0-15)
-const int pwmFrequency_left = 5000;  // Frequency in Hz
-const int pwmResolution_left = 8;    // Resolution in bits (0-255)
+// LEFT FRONT MOTOR
+#define   LEFT_AIN1  27
+#define   LEFT_AIN2  14
+
+// LEFT REAR MOTOR
+#define   LEFT_BIN1  26
+#define   LEFT_BIN2  25
 
 // RIGHT FRONT MOTOR
 #define   RIGHT_AIN1  19
@@ -31,13 +39,46 @@ const int pwmResolution_left = 8;    // Resolution in bits (0-255)
 #define   RIGHT_BIN1  21
 #define   RIGHT_BIN2  22
 
-// LEFT FRONT MOTOR
-#define   LEFT_AIN1  27
-#define   LEFT_AIN2  14
+// Speed and Delay Duration Declaration
+#define MID_SPEED 100    
+#define HIGH_SPEED 120    
+#define LOW_SPEED 90
+#define LONG_DELAY_TIME 110 
+#define DELAY_TIME 80 
+#define SHORT_DELAY_TIME 70 
 
-// LEFT REAR MOTOR
-#define   LEFT_BIN1  26
-#define   LEFT_BIN2  25
+// Motor Driver Function Declaration
+void FL_fwd(int speed);                                 // Front Left Rotate Clockwise
+void FL_rev(int speed);                                 // Front Left Rotate Counter Clockwise
+void RL_fwd(int speed);                                 // Rear Left Rotate Clockwise
+void RL_rev(int speed);                                 // Rear Left Rotate Counter Clockwise
+void FR_fwd(int speed);                                 // Front Right Rotate Clockwise
+void FR_rev(int speed);                                 // Front Right Rotate Counter Clockwise
+void RR_fwd(int speed);                                 // Rear Right Rotate Clockwise
+void RR_rev(int speed);                                 // Rear RIght Rotate Counter Clockwise
+void brake(int speed);                                  // Stop all motor
+
+// Standard Wheel Movement Function Declaration
+void std_fwd(int speed);                                  // Standard Wheel Move Foward 
+void std_rev(int speed);                                  // Standard Wheel Reverse
+void std_left_fwd(int speed_1, int speed_);               // Standard Wheel Move Forward Left
+void std_right_fwd(int speed_1, int speed_);              // Standard Wheel Move Forward Right
+void std_left_rev(int speed_1, int speed_);               // Standard Wheel Reverse Left
+void std_right_rev(int speed_1, int speed_);              // Standard Wheel Reverse Right
+
+// Mecanum Wheel Movement Function Declaration
+void mecanum_move_fwd(int speed);           // Mecanum Wheel Move Foward
+void mecanum_move_rev(int speed);           // Mecanum Wheel Reverse
+void mecanum_shift_left(int speed);         // Mecanum Wheel Shift to the Left
+void mecanum_shift_right(int speed);        // Mecanum Wheel Shift to the Right
+void mecanum_turn_left(int speed);          // Mecanum Wheel Turn to the Left
+void mecanum_turn_right(int speed);         // Mecanum Wheel Turn to the Right
+void mecanum_left_fwd(int speed);           // Mecanum Wheel Move to the Left
+void mecanum_right_fwd(int speed);          // Mecanum Wheel Move to the Right
+void mecanum_left_rev(int speed);           // Mecanum Wheel Reverse Left
+void mecanum_right_rev(int speed);          // Mecanum Wheel Reverse Right
+void mecanum_sharp_right_turn(int speed);   // Mecanum Wheel Move Sharp Right
+void mecanum_sharp_left_turn(int speed);    // Mecanum Wheel Move Sharp Left
 
 void setup() 
 {
@@ -69,5 +110,147 @@ void setup()
 
 void loop() 
 {
+
+}
+
+// Motor Driver Function
+void FL_fwd(int speed){
+  digitalWrite(LEFT_AIN1,1);
+  digitalWrite(LEFT_AIN2,0);
+  ledcWrite(pwmChannel_left, speed);
+}
+
+void FL_rev(int speed){
+  digitalWrite(LEFT_AIN1,0);
+  digitalWrite(LEFT_AIN2,1);
+  ledcWrite(pwmChannel_left, speed);
+}
+
+void RL_fwd(int speed){
+  digitalWrite(LEFT_BIN1,1);
+  digitalWrite(LEFT_BIN2,0);
+  ledcWrite(pwmChannel_left, speed);
+}
+
+void RL_rev(int speed){
+  digitalWrite(LEFT_BIN1,0);
+  digitalWrite(LEFT_BIN2,1);
+  ledcWrite(pwmChannel_left, speed);
+}
+
+void FR_fwd(int speed){
+  digitalWrite(RIGHT_AIN1,1);
+  digitalWrite(RIGHT_AIN2,0);
+  ledcWrite(pwmChannel_right, speed);
+}
+
+void FR_rev(int speed){
+  digitalWrite(RIGHT_AIN1,0);
+  digitalWrite(RIGHT_AIN2,1);
+  ledcWrite(pwmChannel_right, speed);
+}
+
+void RR_fwd(int speed){
+  digitalWrite(RIGHT_BIN1,1);
+  digitalWrite(RIGHT_BIN2,0);
+  ledcWrite(pwmChannel_right, speed);
+}
+
+void RR_rev(int speed){
+  digitalWrite(RIGHT_BIN1,0);
+  digitalWrite(RIGHT_BIN2,1);
+  ledcWrite(pwmChannel_right, speed);
+}
+
+void brake(int speed){
+  digitalWrite(LEFT_AIN1,1);
+  digitalWrite(LEFT_AIN2,1);  
+  digitalWrite(LEFT_BIN1,1);
+  digitalWrite(LEFT_BIN2,1);
+  ledcWrite(pwmChannel_left, speed);  
+  digitalWrite(RIGHT_AIN1,1);
+  digitalWrite(RIGHT_AIN2,1);
+  digitalWrite(RIGHT_BIN1,1);
+  digitalWrite(RIGHT_BIN2,1);
+  ledcWrite(pwmChannel_right, speed);  
+}
+
+// Standard Wheel Movement Function
+void std_fwd(int speed){
+  FL_fwd(speed);
+  RL_fwd(speed);
+  FR_fwd(speed);
+  RR_fwd(speed);
+}
+
+void std_rev(int speed){
+  FL_rev(speed);
+  RL_rev(speed);
+  FR_rev(speed);
+  RR_rev(speed);
+}
+
+void std_left_fwd(int speed_1, int speed_2){
+
+}
+
+void std_right_fwd(int speed_1, int speed_){
+
+}
+
+void std_left_rev(int speed_1, int speed_){
+
+}
+
+void std_right_rev(int speed_1, int speed_){
+
+}
+
+// Mecanum Wheel Movement Function
+void mecanum_move_fwd(int speed){
+
+}
+
+void mecanum_move_rev(int speed){
+
+}
+
+void mecanum_shift_left(int speed){
+  
+}
+
+void mecanum_shift_right(int speed){
+  
+}
+
+void mecanum_turn_left(int speed){
+
+}
+
+void mecanum_turn_right(int speed){
+
+}
+
+void mecanum_left_fwd(int speed){
+  
+}
+
+void mecanum_right_fwd(int speed){
+
+}
+
+void mecanum_left_rev(int speed){
+
+}
+
+void mecanum_right_rev(int speed){
+
+}
+
+void mecanum_sharp_right_turn(int speed){
+  
+}
+
+void mecanum_sharp_left_turn(int speed){
 
 }
